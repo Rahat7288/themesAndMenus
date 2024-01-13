960d43ab4fb88c223657a9dac660fd30914482c4
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-01-14 00:25:52 +0600

    adding menu items and display the menu item on appbar

--- app/src/main/java/com/example/themesandmenus/MainActivity.kt
+++ app/src/main/java/com/example/themesandmenus/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.themesandmenus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.themesandmenus.R.menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+//    adding menu on the appBar====================
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.example_menu,menu)
+        return true
+    }
+//Actions with the menu items=======================
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.item1 -> {
+                Toast.makeText(this@MainActivity, "Item 1 clicked", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.item_2 -> {
+                Toast.makeText(this@MainActivity, "Item 2 clicked", Toast.LENGTH_SHORT).show()
+                return  true
+        }
+
+            R.id.sub_menu1 -> {
+                Toast.makeText(this@MainActivity, "Sub Item 1 clicked", Toast.LENGTH_SHORT).show()
+                return  true
+
+            }
+            R.id.sub_menu2 -> {
+                Toast.makeText(this@MainActivity, "Sub Item 2 clicked", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+
+
     }
 }
